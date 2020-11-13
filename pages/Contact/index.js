8fdb21d21720943ca7b520c14e475b14322f2626
Kasey Jones <joneskm72@gmail.com>
2020-11-12 20:28:51 -0500

    Updated contact form

---
+++ pages/Contact/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+<Form>
+  <Form.Row>
+    <Form.Group as={Col} controlId="formGridFirst">
+      <Form.Label>First Name</Form.Label>
+      <Form.Control type="first" placeholder="First Name" />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridLast">
+      <Form.Label>Last Name</Form.Label>
+      <Form.Control type="last" placeholder="Last Name" />
+    </Form.Group>
+  </Form.Row>
+
+  <Form.Group controlId="formGridEmail">
+    <Form.Label>Email Address</Form.Label>
+    <Form.Control placeholder="Email" />
+  </Form.Group>
+
+  <Form.Row>
+    <Form.Group controlId="formGridMessage">
+      <Form.Label>Message</Form.Label>
+      <Form.Control placeholder="Message" />
+    </Form.Group>
+  </Form.Row>
+
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
