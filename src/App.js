8bc3df825dc613c4a660363a20b399f00057870d
Kasey Jones <joneskm72@gmail.com>
2020-11-12 21:24:12 -0500

    Cleaned up imports

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Project from './components/Project';
+import Contact from './pages/Contact';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Contact />
     </div>
   );
 }
--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,32 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
 
-<Form>
-  <Form.Row>
-    <Form.Group as={Col} controlId="formGridFirst">
-      <Form.Label>First Name</Form.Label>
-      <Form.Control type="first" placeholder="First Name" />
-    </Form.Group>
+class Contact extends Component {
+  render() {
+    return (
+      <Form>
+        <Form.Row>
+          <Form.Group as={Col} controlId="formGridFirst">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control type="first" placeholder="First Name" />
+          </Form.Group>
 
-    <Form.Group as={Col} controlId="formGridLast">
-      <Form.Label>Last Name</Form.Label>
-      <Form.Control type="last" placeholder="Last Name" />
-    </Form.Group>
-  </Form.Row>
+          <Form.Group as={Col} controlId="formGridLast">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control type="last" placeholder="Last Name" />
+          </Form.Group>
+        </Form.Row>
 
-  <Form.Group controlId="formGridEmail">
-    <Form.Label>Email Address</Form.Label>
-    <Form.Control placeholder="Email" />
-  </Form.Group>
+        <Form.Row>
+          <Form.Group controlId="formGridEmail">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control placeholder="Email" />
+          </Form.Group>
+        </Form.Row>
 
-  <Form.Row>
-    <Form.Group controlId="formGridMessage">
-      <Form.Label>Message</Form.Label>
-      <Form.Control placeholder="Message" />
-    </Form.Group>
-  </Form.Row>
+        <Form.Row>
+          <Form.Group controlId="formGridMessage">
+            <Form.Label>Message</Form.Label>
+            <Form.Control placeholder="Message" />
+          </Form.Group>
+        </Form.Row>
 
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</Form>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    )
+  }
+}
+
+export default Contact;
