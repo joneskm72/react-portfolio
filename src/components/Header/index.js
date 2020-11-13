01e88921c40e6f1148cdb92701e93b05ab62ca6e
Kasey Jones <joneskm72@gmail.com>
2020-11-13 15:10:06 -0500

    Testing route issues

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,13 +7,13 @@ class Header extends Component {
   render() {
     return (
       <Navbar bg="dark" variant="dark" expand="lg">
-        <Navbar.Brand href="/home">Kasey Jones</Navbar.Brand>
+        <Navbar.Brand href="#home">Kasey Jones</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="nav-links">
-            <Nav.Link href="/home">About</Nav.Link>
-            <Nav.Link href="/my-portfolio">Portfolio</Nav.Link>
-            <Nav.Link href="/contact-me">Contact</Nav.Link>
+            <Nav.Link href="#home">About</Nav.Link>
+            <Nav.Link href="#my-portfolio">Portfolio</Nav.Link>
+            <Nav.Link href="#contact-me">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
