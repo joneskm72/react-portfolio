e4f76693fce0e0ac6109805dac0f27af0690c10d
Kasey Jones <joneskm72@gmail.com>
2020-11-14 21:57:23 -0500

    Updated portfolio layout

--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -13,55 +13,55 @@ class Portfolio extends Component {
   render() {
     return (
       <div>
-        <Card className='portfolioCard' style={{ width: '30rem' }}>
+        <Card className='portfolioCard' style={{ width: '25rem', marginLeft: '20px', marginRight: '20px', marginTop: '20px', marginBottom: '20px' }}>
           <Card.Img variant="top" src={DayPlanner} href='https://joneskm72.github.io/day-scheduler-hw5/' />
           <Card.Body>
-            <Card.Title className='card-title'>Day Planner</Card.Title>
-            <Card.Text>
+            <Card.Title style={{ textAlign: 'center' }}>Day Planner</Card.Title>
+            <Card.Text style={{ textAlign: 'center' }}>
               A simple calendar application that allows the user to save events for each hour of the day.
             </Card.Text>
             <Button className='btn btn-dark' variant="https://joneskm72.github.io/day-scheduler-hw5/">Day Planner</Button>
           </Card.Body>
         </Card>
 
-        <Card style={{ width: '18rem' }}>
+        <Card style={{ width: '25rem', marginLeft: '20px', marginRight: '20px', marginTop: '20px', marginBottom: '20px' }}>
           <Card.Img variant="top" src={DreamDog} href='https://vkalaparthy.github.io/2020-Project1/' />
           <Card.Body>
-            <Card.Title>Dream Dog</Card.Title>
-            <Card.Text>
+            <Card.Title style={{ textAlign: 'center' }}>Dream Dog</Card.Title>
+            <Card.Text style={{ textAlign: 'center' }}>
               A simple calendar application that allows the user to save events for each hour of the day.
             </Card.Text>
             <Button className='btn btn-dark' variant="https://vkalaparthy.github.io/2020-Project1/">Dream Dog</Button>
           </Card.Body>
         </Card>
 
-        <Card style={{ width: '18rem' }}>
+        <Card style={{ width: '25rem', marginLeft: '20px', marginRight: '20px', marginTop: '20px', marginBottom: '20px' }}>
           <Card.Img variant="top" src={PasswordGenerator} href='https://joneskm72.github.io/password-generator-hw3/' />
           <Card.Body>
-            <Card.Title>Password Generator</Card.Title>
-            <Card.Text>
+            <Card.Title style={{ textAlign: 'center' }}>Password Generator</Card.Title>
+            <Card.Text style={{ textAlign: 'center' }}>
               A simple calendar application that allows the user to save events for each hour of the day.
             </Card.Text>
             <Button className='btn btn-dark' variant="https://joneskm72.github.io/password-generator-hw3/">Password Generator</Button>
           </Card.Body>
         </Card>
 
-        <Card style={{ width: '18rem' }}>
+        <Card style={{ width: '25rem', marginLeft: '20px', marginRight: '20px', marginTop: '20px', marginBottom: '20px' }}>
           <Card.Img variant="top" src={WeatherDashboard} href='https://joneskm72.github.io/server-side-apis-hw6/' />
           <Card.Body>
-            <Card.Title>Weather Dashboard</Card.Title>
-            <Card.Text>
+            <Card.Title style={{ textAlign: 'center' }}>Weather Dashboard</Card.Title>
+            <Card.Text style={{ textAlign: 'center' }}>
               A simple calendar application that allows the user to save events for each hour of the day.
             </Card.Text>
             <Button className='btn btn-dark' variant="https://joneskm72.github.io/server-side-apis-hw6/">Weather Dashboard</Button>
           </Card.Body>
         </Card>
 
-        <Card style={{ width: '18rem' }}>
+        <Card style={{ width: '25rem', marginLeft: '20px', marginRight: '20px', marginTop: '20px', marginBottom: '20px' }}>
           <Card.Img variant="top" src={EventSafe} href='https://project-2-team-3.herokuapp.com/' />
           <Card.Body>
-            <Card.Title>EventSafe</Card.Title>
-            <Card.Text>
+            <Card.Title style={{ textAlign: 'center' }}>EventSafe</Card.Title>
+            <Card.Text style={{ textAlign: 'center' }}>
               A simple calendar application that allows the user to save events for each hour of the day.
             </Card.Text>
             <Button className='btn btn-dark' variant="https://project-2-team-3.herokuapp.com/">EventSafe</Button>
