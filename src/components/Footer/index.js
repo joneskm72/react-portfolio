d49d1fb53b1972fb0ba84646069cf37732e13d41
Kasey Jones <joneskm72@gmail.com>
2020-11-15 22:40:04 -0500

    Updated footer format

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,35 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
 import "./style.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 
-function Footer() {
-  return (
-    <div class="footer bg-dark">
-      <div class="container">
-            <div class="row justify-content-md-center">
-                <div class="col-6 col-lg-3">
-                    <i class="fas fa-phone"></i>
-                    &nbsp;828-406-5140
-                </div>
-                <div class="col-6 col-lg-2">
-                    <i class="far fa-envelope"></i>
-                    <a href= "mailto:jones.m.kasey@gmail.com">&nbsp;Email</a>
-                </div>
-                <div class="col-6 col-lg-2">
-                    <i class="fab fa-linkedin"></i>
-                    <a href= "https://www.linkedin.com/in/kasey-m-jones">&nbsp;LinkedIn</a>
-                </div>
-                <div class="col-6 col-lg-2">
-                    <fab icon="github" />
-                    <a href="https://github.com/joneskm72">&nbsp;GitHub</a>
-                </div>
-                <div class="col-6 col-lg-2">
-                    <i class="fa fa-file-text"></i>
-                    <a href="https://docs.google.com/document/d/1tyUOv-2P8FZdMmhxAmREY9vBbaZ82G2wYEOEoP-jOjI/edit?usp=sharing">&nbsp;Resume</a>
-                </div>
-            </div>
-        </div>
-    </div>
-  );
+library.add(fab)
+
+class Footer extends Component {
+  render() {
+    return (
+      <div class="footer bg-dark">
+        <Container>
+              <Row>
+                  <Col xs={6} lg={3}>
+                      <i class="fas fa-phone"></i>
+                      &nbsp;828-406-5140
+                  </Col>
+                  <Col xs={6} lg={2}>
+                      <i class="far fa-envelope"></i>
+                      <a href= "mailto:jones.m.kasey@gmail.com">&nbsp;Email</a>
+                  </Col>
+                  <Col xs={6} lg={2}>
+                      <i class="fab fa-linkedin"></i>
+                      <a href= "https://www.linkedin.com/in/kasey-m-jones">&nbsp;LinkedIn</a>
+                  </Col>
+                  <Col xs={6} lg={2}>
+                      <fab icon="github" />
+                      <a href="https://github.com/joneskm72">&nbsp;GitHub</a>
+                  </Col>
+                  <Col xs={6} lg={2}>
+                      <i class="fa fa-file-text"></i>
+                      <a href="https://docs.google.com/document/d/1tyUOv-2P8FZdMmhxAmREY9vBbaZ82G2wYEOEoP-jOjI/edit?usp=sharing">&nbsp;Resume</a>
+                  </Col>
+              </Row>
+          </Container>
+      </div>
+    );
+  }
 }
 
 export default Footer;
