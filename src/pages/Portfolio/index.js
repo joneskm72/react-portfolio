ea5114b13e3af509123e12638fb076d54e7bbb9e
joneskm72 <joneskm72@gmail.com>
2020-12-18 17:37:44 -0500

    Updated card spacing

--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -17,62 +17,70 @@ const newTab = (url) => {
 class Portfolio extends Component {
   render() {
     return (
-      <div style={{display: 'flex', flexDirection: 'row'}}>
-        <Card className='portfolioCard' style={{ width: '25rem', marginLeft: '20px', marginRight: '20px', marginTop: '20px', marginBottom: '20px' }}>
-          <Card.Img variant="top" src={DayPlanner} onClick={() => newTab('https://joneskm72.github.io/day-scheduler-hw5/')}/>
-          <Card.Body>
-            <Card.Title style={{ textAlign: 'center' }}>Day Planner</Card.Title>
-            <Card.Text style={{ textAlign: 'center' }}>
-              A simple calendar application that allows the user to save events for each hour of the day.
-            </Card.Text>
-            <Button className='btn btn-dark' onClick={() => newTab('https://joneskm72.github.io/day-scheduler-hw5/')}>Day Planner</Button>
-          </Card.Body>
-        </Card>
-
-        <Card style={{ width: '25rem', marginLeft: '20px', marginRight: '20px', marginTop: '20px', marginBottom: '20px' }}>
-          <Card.Img variant="top" src={DreamDog} onClick={() => newTab('https://vkalaparthy.github.io/2020-Project1/')}/>
-          <Card.Body>
-            <Card.Title style={{ textAlign: 'center' }}>Dream Dog</Card.Title>
-            <Card.Text style={{ textAlign: 'center' }}>
-              A simple calendar application that allows the user to save events for each hour of the day.
-            </Card.Text>
-            <Button className='btn btn-dark' onClick={() => newTab('https://vkalaparthy.github.io/2020-Project1/')}>Dream Dog</Button>
-          </Card.Body>
-        </Card>
-
-        <Card style={{ width: '25rem', marginLeft: '20px', marginRight: '20px', marginTop: '20px', marginBottom: '20px' }}>
-          <Card.Img variant="top" src={PasswordGenerator} onClick={() => newTab('https://joneskm72.github.io/password-generator-hw3/')}/>
-          <Card.Body>
-            <Card.Title style={{ textAlign: 'center' }}>Password Generator</Card.Title>
-            <Card.Text style={{ textAlign: 'center' }}>
-              A simple calendar application that allows the user to save events for each hour of the day.
-            </Card.Text>
-            <Button className='btn btn-dark' onClick={() => newTab('https://joneskm72.github.io/password-generator-hw3/')}>Password Generator</Button>
-          </Card.Body>
-        </Card>
-
-        <Card style={{ width: '25rem', marginLeft: '20px', marginRight: '20px', marginTop: '20px', marginBottom: '20px' }}>
-          <Card.Img variant="top" src={WeatherDashboard} onClick={() => newTab('https://joneskm72.github.io/server-side-apis-hw6/')}/>
-          <Card.Body>
-            <Card.Title style={{ textAlign: 'center' }}>Weather Dashboard</Card.Title>
-            <Card.Text style={{ textAlign: 'center' }}>
-              A simple calendar application that allows the user to save events for each hour of the day.
-            </Card.Text>
-            <Button className='btn btn-dark' onClick={() => newTab('https://joneskm72.github.io/server-side-apis-hw6/')}>Weather Dashboard</Button>
-          </Card.Body>
-        </Card>
-
-        <Card style={{ width: '25rem', marginLeft: '20px', marginRight: '20px', marginTop: '20px', marginBottom: '20px' }}>
-          <Card.Img variant="top" src={EventSafe} onClick={() => newTab('https://project-2-team-3.herokuapp.com/')}/>
-          <Card.Body>
-            <Card.Title style={{ textAlign: 'center' }}>EventSafe</Card.Title>
-            <Card.Text style={{ textAlign: 'center' }}>
-              A simple calendar application that allows the user to save events for each hour of the day.
-            </Card.Text>
-            <Button className='btn btn-dark' onClick={() => newTab('https://project-2-team-3.herokuapp.com/')}>EventSafe</Button>
-          </Card.Body>
-        </Card>
-      </div>
+      <div className="container portfolioContainer">
+        <div className="row portfolioRow">
+          <div className="portfolioCard col-xl-4 col-md-6 col-sm-12">
+            <Card className='cardStyle'>
+              <Card.Img className='portfolioImage' variant="top" src={DayPlanner} onClick={() => newTab('https://joneskm72.github.io/day-scheduler-hw5/')}/>
+              <Card.Body>
+                <Card.Title style={{ textAlign: 'center' }}>Day Planner</Card.Title>
+                <Card.Text style={{ textAlign: 'center' }}>
+                  A simple calendar application that allows the user to save events for each hour of the day.
+                </Card.Text>
+                <Button className='btn btn-dark' onClick={() => newTab('https://joneskm72.github.io/day-scheduler-hw5/')}>View</Button>
+              </Card.Body>
+            </Card>
+          </div>
+          <div className="portfolioCard col-xl-4 col-md-6 col-sm-12">
+            <Card className='cardStyle'>
+              <Card.Img className='portfolioImage' variant="top" src={DreamDog} onClick={() => newTab('https://vkalaparthy.github.io/2020-Project1/')}/>
+              <Card.Body>
+                <Card.Title style={{ textAlign: 'center' }}>Dream Dog</Card.Title>
+                <Card.Text style={{ textAlign: 'center' }}>
+                  A simple calendar application that allows the user to save events for each hour of the day.
+                </Card.Text>
+                <Button className='btn btn-dark' onClick={() => newTab('https://vkalaparthy.github.io/2020-Project1/')}>View</Button>
+              </Card.Body>
+            </Card>
+          </div>
+          <div className="portfolioCard col-xl-4 col-md-6 col-sm-12">
+            <Card className='cardStyle'>
+              <Card.Img className='portfolioImage' variant="top" src={PasswordGenerator} onClick={() => newTab('https://joneskm72.github.io/password-generator-hw3/')}/>
+              <Card.Body>
+                <Card.Title style={{ textAlign: 'center' }}>Password Generator</Card.Title>
+                <Card.Text style={{ textAlign: 'center' }}>
+                  An application that enables users to generate a secure password.
+                </Card.Text>
+                <Button className='btn btn-dark' onClick={() => newTab('https://joneskm72.github.io/password-generator-hw3/')}>View</Button>
+              </Card.Body>
+            </Card>
+          </div>
+          <div className="portfolioCard col-xl-4 col-md-6 col-sm-12">
+            <Card className='cardStyle'>
+              <Card.Img className='portfolioImage' variant="top" src={WeatherDashboard} onClick={() => newTab('https://joneskm72.github.io/server-side-apis-hw6/')}/>
+              <Card.Body>
+                <Card.Title style={{ textAlign: 'center' }}>Weather Dashboard</Card.Title>
+                <Card.Text style={{ textAlign: 'center' }}>
+                  A simple calendar application that allows the user to save events for each hour of the day.
+                </Card.Text>
+                <Button className='btn btn-dark' onClick={() => newTab('https://joneskm72.github.io/server-side-apis-hw6/')}>View</Button>
+              </Card.Body>
+            </Card>
+          </div>
+          <div className="portfolioCard col-xl-4 col-md-6 col-sm-12">
+            <Card className='cardStyle'>
+              <Card.Img className='portfolioImage' variant="top" src={EventSafe} onClick={() => newTab('https://project-2-team-3.herokuapp.com/')}/>
+              <Card.Body>
+                <Card.Title style={{ textAlign: 'center' }}>EventSafe</Card.Title>
+                <Card.Text style={{ textAlign: 'center' }}>
+                  A simple calendar application that allows the user to save events for each hour of the day.
+                </Card.Text>
+                <Button className='btn btn-dark' onClick={() => newTab('https://project-2-team-3.herokuapp.com/')}>View</Button>
+              </Card.Body>
+            </Card>
+          </div>
+        </div>
+      </div>  
     )
   }
 }
