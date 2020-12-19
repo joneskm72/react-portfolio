75229f34a53e9690ba9e8a0bfb51ab2873970124
joneskm72 <joneskm72@gmail.com>
2020-12-19 18:57:53 -0500

    Updated column sizes

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -3,44 +3,45 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
+import './style.css'
 
 class Contact extends Component {
   render() {
     return (
       <Form>
-        <h1>Contact</h1>
+        <h1 className='formHeader'>Contact</h1>
         <Form.Row className='formRow'>
-          <Form.Group className='formGroup col-md-3' as={Col} controlId="formGridFirst">
+          <Form.Group className='formGroup col-lg-3 col-md-6 col-sm-12' as={Col} controlId="formGridFirst">
             <Form.Label>First Name</Form.Label>
             <Form.Control type="first" placeholder="First Name" />
           </Form.Group>
 
-          <Form.Group className='formGroup col-md-3' as={Col} controlId="formGridLast">
+          <Form.Group className='formGroup col-lg-3 col-md-6 col-sm-12' as={Col} controlId="formGridLast">
             <Form.Label>Last Name</Form.Label>
             <Form.Control type="last" placeholder="Last Name" />
           </Form.Group>
         </Form.Row>
 
         <Form.Row>
-          <Form.Group className='formGroup col-md-3' controlId="formGridEmail">
+          <Form.Group className='formGroup col-lg-3 col-md-6 col-sm-12' controlId="formGridEmail">
             <Form.Label>Email Address</Form.Label>
             <Form.Control placeholder="Email" />
           </Form.Group>
 
-          <Form.Group className='formGroup col-md-3' controlId="formGridCompany">
+          <Form.Group className='formGroup col-lg-3 col-md-6 col-sm-12' controlId="formGridCompany">
             <Form.Label>Company</Form.Label>
             <Form.Control placeholder="Company" />
           </Form.Group>
         </Form.Row>
 
         <Form.Row>
-          <Form.Group className='formGroup col-md-6' controlId="formGridMessage">
+          <Form.Group className='formGroup col-md-6 col-sm-12' controlId="exampleForm.ControlTextarea1">
             <Form.Label>Message</Form.Label>
-            <Form.Control placeholder="Message" />
+            <Form.Control as="textarea" rows={3} />
           </Form.Group>
         </Form.Row>
 
-        <Button className='formButton' variant="primary" type="submit">
+        <Button className='formButton btn-dark' variant="primary" type="submit">
           Submit
         </Button>
       </Form>
