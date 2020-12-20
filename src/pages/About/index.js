cf62a006226be49976fa43303d3880c1141fc01d
joneskm72 <joneskm72@gmail.com>
2020-12-20 14:49:35 -0500

    Restyled about page and added background image

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,23 +1,27 @@
 import React, { Component } from 'react';
 import Profile from '../../Assets/Images/picture.png';
+import Keyboard from '../../Assets/Images/keyboard.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
+import './style.css';
 
 class About extends Component {
   render() {
     return (
-      <div>
-        <h1 style={{textAlign: 'center'}}>About Me</h1>
+      <div className='aboutBackground' style={{backgroundImage: `url(${Keyboard})`}}>
+        <h1 className='aboutHeader'>About Me</h1>
         <Container>
           <Row>
-            <Col xs={8} md={4} style={{ flexDirection: 'column', margin: 'auto', alignItems: 'left', justifyContent: 'left' }} >
-              <Image src={ Profile } />
+            <Col className='aboutCol1' xs={12} md={3}>
+              <Image className='profileImage' src={ Profile } />
             </Col>
-            <Col xs={8} md={4} style={{ flexDirection: 'column', margin: 'auto', alignItems: 'right', justifyContent: 'right' }} >
-              <p style={{ textAlign: 'center' }}>An aspiring Test Engineer who has experience as a Client Support Representative with a proven record of maintaining client loyalty and providing the highest level of customer experience. Passionate about helping people reach their goals and achieve their fullest potential.</p>
+          </Row>
+          <Row>
+            <Col className='aboutCol2' xs={12} md={12}>
+              <p className='colP'>An aspiring Test Engineer who has experience as a Client Support Representative with a proven record of maintaining client loyalty and providing the highest level of customer experience. Passionate about helping people reach their goals and achieve their fullest potential.</p>
             </Col>
           </Row>
         </Container>
--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -8,43 +8,45 @@ import './style.css'
 class Contact extends Component {
   render() {
     return (
-      <Form>
-        <h1 className='formHeader'>Contact</h1>
-        <Form.Row className='formRow'>
-          <Form.Group className='formGroup col-lg-3 col-md-6 col-sm-12' as={Col} controlId="formGridFirst">
-            <Form.Label>First Name</Form.Label>
-            <Form.Control type="first" placeholder="First Name" />
-          </Form.Group>
+      <div className= 'container contactContainer'>
+        <Form>
+          <h1 className='formHeader'>Contact</h1>
+          <Form.Row className='row formRow'>
+            <Form.Group className='formGroup col-lg-3 col-md-6 col-sm-12' as={Col} controlId="formGridFirst">
+              <Form.Label>First Name</Form.Label>
+              <Form.Control type="first" placeholder="First Name" />
+            </Form.Group>
 
-          <Form.Group className='formGroup col-lg-3 col-md-6 col-sm-12' as={Col} controlId="formGridLast">
-            <Form.Label>Last Name</Form.Label>
-            <Form.Control type="last" placeholder="Last Name" />
-          </Form.Group>
-        </Form.Row>
+            <Form.Group className='formGroup col-lg-3 col-md-6 col-sm-12' as={Col} controlId="formGridLast">
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control type="last" placeholder="Last Name" />
+            </Form.Group>
+          </Form.Row>
+          
+          <Form.Row>
+            <Form.Group className='formGroup col-lg-3 col-md-6 col-sm-12' controlId="formGridEmail">
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control placeholder="Email" />
+            </Form.Group>
 
-        <Form.Row>
-          <Form.Group className='formGroup col-lg-3 col-md-6 col-sm-12' controlId="formGridEmail">
-            <Form.Label>Email Address</Form.Label>
-            <Form.Control placeholder="Email" />
-          </Form.Group>
+            <Form.Group className='formGroup col-lg-3 col-md-6 col-sm-12' controlId="formGridCompany">
+              <Form.Label>Company Name</Form.Label>
+              <Form.Control placeholder="Company" />
+            </Form.Group>
+          </Form.Row>
 
-          <Form.Group className='formGroup col-lg-3 col-md-6 col-sm-12' controlId="formGridCompany">
-            <Form.Label>Company</Form.Label>
-            <Form.Control placeholder="Company" />
-          </Form.Group>
-        </Form.Row>
+          <Form.Row>
+            <Form.Group className='formGroup col-md-6 col-sm-12' controlId="exampleForm.ControlTextarea1">
+              <Form.Label>Message</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+          </Form.Row>
 
-        <Form.Row>
-          <Form.Group className='formGroup col-md-6 col-sm-12' controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Message</Form.Label>
-            <Form.Control as="textarea" rows={3} />
-          </Form.Group>
-        </Form.Row>
-
-        <Button className='formButton btn-dark' variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+          <Button className='formButton btn-dark' variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     )
   }
 }
