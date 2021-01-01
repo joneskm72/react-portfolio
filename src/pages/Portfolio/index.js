b9d79601ac522aa1ebdb37f253893e13f3602b4a
joneskm72 <joneskm72@gmail.com>
2021-01-01 17:56:17 -0500

    Added GitHub buttons and several new projects

--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -21,53 +21,76 @@ class Portfolio extends Component {
         <div className="row portfolioRow">
           <div className="portfolioCard col-xl-4 col-md-6 col-sm-12">
             <Card className='cardStyle'>
-              <Card.Img className='portfolioImage' variant="top" src={DayPlanner} onClick={() => newTab('https://joneskm72.github.io/day-scheduler-hw5/')}/>
+              <Card.Img className='portfolioImage' variant="top" src={WeatherDashboard} onClick={() => newTab('https://jamez.herokuapp.com/')}/>
               <Card.Body>
-                <Card.Title style={{ textAlign: 'center' }}>Day Planner</Card.Title>
+                <Card.Title style={{ textAlign: 'center' }}>jamEZ</Card.Title>
                 <Card.Text style={{ textAlign: 'center' }}>
-                  A simple calendar application that allows the user to save events for each hour of the day.
+                An application that allows users to browse artists and songs, and create a playlist.
                 </Card.Text>
-                <Button className='btn btn-dark' onClick={() => newTab('https://joneskm72.github.io/day-scheduler-hw5/')}>View</Button>
-                <Button className='btn btn-dark' onClick={() => newTab('https://github.com/joneskm72/day-scheduler-hw5')}>GitHub</Button>
+                <div className='portfolioButton col-lg-12 col-md-4 col-sm-5'>
+                  <Button className='btn btn-dark' onClick={() => newTab('https://jamez.herokuapp.com/')}>View</Button>
+                </div>
+                <div className='portfolioButton col-lg-12 col-md-4 col-sm-5>'>
+                  <Button className='btn btn-dark' onClick={() => newTab('https://github.com/joneskm72/project3-repo')}>GitHub</Button>
+                </div>
               </Card.Body>
             </Card>
           </div>
+
           <div className="portfolioCard col-xl-4 col-md-6 col-sm-12">
             <Card className='cardStyle'>
-              <Card.Img className='portfolioImage' variant="top" src={DreamDog} onClick={() => newTab('https://vkalaparthy.github.io/2020-Project1/')}/>
+              <Card.Img className='portfolioImage' variant="top" src={WeatherDashboard} onClick={() => newTab('https://workout-tracker-kasey.herokuapp.com')}/>
               <Card.Body>
-                <Card.Title style={{ textAlign: 'center' }}>Dream Dog</Card.Title>
+                <Card.Title style={{ textAlign: 'center' }}>Fitness Tracker</Card.Title>
                 <Card.Text style={{ textAlign: 'center' }}>
-                An application that allows users to search for and adopt dogs.
+                An application that allows users to view, create, and track daily workouts.
                 </Card.Text>
-                <Button className='btn btn-dark' onClick={() => newTab('https://vkalaparthy.github.io/2020-Project1/')}>View</Button>
+                <div className='portfolioButton col-lg-12 col-md-4 col-sm-5'>
+                  <Button className='btn btn-dark' onClick={() => newTab('https://workout-tracker-kasey.herokuapp.com')}>View</Button>
+                </div>
+                <div className='portfolioButton col-lg-12 col-md-4 col-sm-5>'>
+                  <Button className='btn btn-dark' onClick={() => newTab('https://github.com/joneskm72/workout-tracker')}>GitHub</Button>
+                </div>
               </Card.Body>
             </Card>
           </div>
+
           <div className="portfolioCard col-xl-4 col-md-6 col-sm-12">
             <Card className='cardStyle'>
-              <Card.Img className='portfolioImage' variant="top" src={PasswordGenerator} onClick={() => newTab('https://joneskm72.github.io/password-generator-hw3/')}/>
+              <Card.Img className='portfolioImage' variant="top" src={WeatherDashboard} onClick={() => newTab('https://joneskm72.github.io/employee-directory/')}/>
               <Card.Body>
-                <Card.Title style={{ textAlign: 'center' }}>Password Generator</Card.Title>
+                <Card.Title style={{ textAlign: 'center' }}>Employee Directory</Card.Title>
                 <Card.Text style={{ textAlign: 'center' }}>
-                  An application that enables users to generate a secure password.
+                An application that, using React, allows users to view an employee directory.
                 </Card.Text>
-                <Button className='btn btn-dark' onClick={() => newTab('https://joneskm72.github.io/password-generator-hw3/')}>View</Button>
+                <div className='portfolioButton col-lg-12 col-md-4 col-sm-5'>
+                  <Button className='btn btn-dark' onClick={() => newTab('https://joneskm72.github.io/employee-directory/')}>View</Button>
+                </div>
+                <div className='portfolioButton col-lg-12 col-md-4 col-sm-5>'>
+                  <Button className='btn btn-dark' onClick={() => newTab('https://github.com/joneskm72/employee-directory')}>GitHub</Button>
+                </div>
               </Card.Body>
             </Card>
           </div>
+
           <div className="portfolioCard col-xl-4 col-md-6 col-sm-12">
             <Card className='cardStyle'>
-              <Card.Img className='portfolioImage' variant="top" src={WeatherDashboard} onClick={() => newTab('https://joneskm72.github.io/server-side-apis-hw6/')}/>
+              <Card.Img className='portfolioImage' variant="top" src={WeatherDashboard} onClick={() => newTab('https://budget-tracker-kasey.herokuapp.com/')}/>
               <Card.Body>
-                <Card.Title style={{ textAlign: 'center' }}>Weather Dashboard</Card.Title>
+                <Card.Title style={{ textAlign: 'center' }}>Budget Tracker</Card.Title>
                 <Card.Text style={{ textAlign: 'center' }}>
-                A site that allows users to find the current and 5-day weather forecast in their city
+                An application that allows users to input expenses and deposits online or offline.
                 </Card.Text>
-                <Button className='btn btn-dark' onClick={() => newTab('https://joneskm72.github.io/server-side-apis-hw6/')}>View</Button>
+                <div className='portfolioButton col-lg-12 col-md-4 col-sm-5'>
+                  <Button className='btn btn-dark' onClick={() => newTab('https://budget-tracker-kasey.herokuapp.com/')}>View</Button>
+                </div>
+                <div className='portfolioButton col-lg-12 col-md-4 col-sm-5>'>
+                  <Button className='btn btn-dark' onClick={() => newTab('https://github.com/joneskm72/Budget-Tracker')}>GitHub</Button>
+                </div>
               </Card.Body>
             </Card>
           </div>
+
           <div className="portfolioCard col-xl-4 col-md-6 col-sm-12">
             <Card className='cardStyle'>
               <Card.Img className='portfolioImage' variant="top" src={EventSafe} onClick={() => newTab('https://project-2-team-3.herokuapp.com/')}/>
@@ -76,7 +99,84 @@ class Portfolio extends Component {
                 <Card.Text style={{ textAlign: 'center' }}>
                   An application that allows users to see local events that follow COVID-19 precautions.
                 </Card.Text>
-                <Button className='btn btn-dark' onClick={() => newTab('https://project-2-team-3.herokuapp.com/')}>View</Button>
+                <div className='portfolioButton col-lg-12 col-md-4 col-sm-5'>
+                  <Button className='btn btn-dark' onClick={() => newTab('https://project-2-team-3.herokuapp.com/')}>View</Button>
+                </div>
+                <div className='portfolioButton col-lg-12 col-md-4 col-sm-5>'>
+                  <Button className='btn btn-dark' onClick={() => newTab('https://github.com/joneskm72/EventSafe')}>GitHub</Button>
+                </div>
+              </Card.Body>
+            </Card>
+          </div>
+
+          <div className="portfolioCard col-xl-4 col-md-6 col-sm-12">
+            <Card className='cardStyle'>
+              <Card.Img className='portfolioImage' variant="top" src={WeatherDashboard} onClick={() => newTab('https://joneskm72.github.io/server-side-apis-hw6/')}/>
+              <Card.Body>
+                <Card.Title style={{ textAlign: 'center' }}>Weather Dashboard</Card.Title>
+                <Card.Text style={{ textAlign: 'center' }}>
+                A site that allows users to find the current and 5-day weather forecast in their city
+                </Card.Text>
+                <div className='portfolioButton col-lg-12 col-md-4 col-sm-5'>
+                  <Button className='btn btn-dark' onClick={() => newTab('https://joneskm72.github.io/server-side-apis-hw6/')}>View</Button>
+                </div>
+                <div className='portfolioButton col-lg-12 col-md-4 col-sm-5>'>
+                  <Button className='btn btn-dark' onClick={() => newTab('https://github.com/joneskm72/weather-dashboard')}>GitHub</Button>
+                </div>
+              </Card.Body>
+            </Card>
+          </div>
+
+          <div className="portfolioCard col-xl-4 col-md-6 col-sm-12">
+            <Card className='cardStyle'>
+              <Card.Img className='portfolioImage' variant="top" src={DayPlanner} onClick={() => newTab('https://joneskm72.github.io/day-scheduler-hw5/')}/>
+              <Card.Body>
+                <Card.Title style={{ textAlign: 'center' }}>Day Planner</Card.Title>
+                <Card.Text style={{ textAlign: 'center' }}>
+                  A simple calendar application that allows the user to save events for each hour of the day.
+                </Card.Text>
+                <div className='portfolioButton col-lg-12 col-md-4 col-sm-5'>
+                  <Button className='btn btn-dark' onClick={() => newTab('https://joneskm72.github.io/day-scheduler-hw5/')}>View</Button>
+                </div>
+                <div className='portfolioButton col-lg-12 col-md-4 col-sm-5>'>
+                  <Button className='btn btn-dark' onClick={() => newTab('https://github.com/joneskm72/day-scheduler-hw5')}>GitHub</Button>
+                </div>
+              </Card.Body>
+            </Card>
+          </div>
+
+          <div className="portfolioCard col-xl-4 col-md-6 col-sm-12">
+            <Card className='cardStyle'>
+              <Card.Img className='portfolioImage' variant="top" src={DreamDog} onClick={() => newTab('https://vkalaparthy.github.io/2020-Project1/')}/>
+              <Card.Body>
+                <Card.Title style={{ textAlign: 'center' }}>Dream Dog</Card.Title>
+                <Card.Text style={{ textAlign: 'center' }}>
+                An application that allows users to search for and adopt dogs.
+                </Card.Text>
+                <div className='portfolioButton col-lg-12 col-md-4 col-sm-5'>
+                  <Button className='btn btn-dark' onClick={() => newTab('https://vkalaparthy.github.io/2020-Project1/')}>View</Button>
+                </div>
+                <div className='portfolioButton col-lg-12 col-md-4 col-sm-5>'>
+                  <Button className='btn btn-dark' onClick={() => newTab('https://github.com/joneskm72/Dream-Dog')}>GitHub</Button>
+                </div>
+              </Card.Body>
+            </Card>
+          </div>
+
+          <div className="portfolioCard col-xl-4 col-md-6 col-sm-12">
+            <Card className='cardStyle'>
+              <Card.Img className='portfolioImage' variant="top" src={PasswordGenerator} onClick={() => newTab('https://joneskm72.github.io/password-generator-hw3/')}/>
+              <Card.Body>
+                <Card.Title style={{ textAlign: 'center' }}>Password Generator</Card.Title>
+                <Card.Text style={{ textAlign: 'center' }}>
+                  An application that enables users to generate a secure password.
+                </Card.Text>
+                <div className='portfolioButton col-lg-12 col-md-4 col-sm-5'>
+                  <Button className='btn btn-dark' onClick={() => newTab('https://joneskm72.github.io/password-generator-hw3/')}>View</Button>
+                </div>
+                <div className='portfolioButton col-lg-12 col-md-4 col-sm-5>'>
+                  <Button className='btn btn-dark' onClick={() => newTab('https://github.com/joneskm72/password-generator-hw3')}>GitHub</Button>
+                </div>
               </Card.Body>
             </Card>
           </div>
