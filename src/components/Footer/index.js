34494cbc284e9bb03666576ace501aa6a281e9fa
joneskm72 <joneskm72@gmail.com>
2021-12-17 14:52:45 -0500

    Updated footer links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -18,15 +18,15 @@ class Footer extends Component {
               <Row>
                   <Col xs={6} lg={3}>
                       <i class="far fa-envelope"></i>
-                      <a href= "mailto:jones.m.kasey@gmail.com">&nbsp;Email</a>
+                      <a href= "mailto:jones.m.kasey@gmail.com" target="_blank" rel="noopener noreferrer">&nbsp;Email</a>
                   </Col>
                   <Col xs={6} lg={3}>
                       <i class="fab fa-linkedin"></i>
-                      <a href= "https://www.linkedin.com/in/kasey-m-jones">&nbsp;LinkedIn</a>
+                      <a href= "https://www.linkedin.com/in/kasey-m-jones" target="_blank">&nbsp;LinkedIn</a>
                   </Col>
                   <Col xs={6} lg={3}>
                       <i class="fab fa-github"></i>
-                      <a href="https://github.com/joneskm72">&nbsp;GitHub</a>
+                      <a href="https://github.com/joneskm72" target="_blank">&nbsp;GitHub</a>
                   </Col>
                   <Col xs={6} lg={3}>
                       <i class="fa fa-file-text"></i>
